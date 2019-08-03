b68261f1ddb7ffad88fc5e5b7a37e896108f8f3f
chengchun <chengchun@guanghe.tv>
2019-08-03 08:35:03 +0800

    modify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import QRCode from 'qrcode.react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QRCode value="https://www.baidu.com/" fgColor="black" includeMargin={true} renderAs="svg" level="H"  size={250} bgColor="white"></QRCode>
     </div>
   );
 }
